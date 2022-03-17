ea921ec426bbb3ed57d7343ad5f20246b6b97ab4
Brandon Cassidy <brandoncassidy323@gmail.com>
2022-03-16 21:18:10 -0400

    Add project files.

---
+++ Brandon_Cassidy_CS559_Assignment2/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brandon_Cassidy_CS559_Assignment2
+{
+    class Person
+    {
+        // Internal fields
+        private string firstName;
+        private string lastName;
+        private DateTime birthDate;
+
+        // Public properties
+        public string FirstName
+        {
+            get { return firstName; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+        }
+    }
+}
---
+++ Brandon_Cassidy_CS559_Assignment2/YourQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace Brandon_Cassidy_CS559_Assignment2
+{
+    class YourQueue : Queue
+    {
+        // Setting up the person dequeued event.
+        public delegate void PersonDequeuedHandler(string Message);
+        public event PersonDequeuedHandler PersonDequeued;
+
+        // Setting up the non-person dequeued event.
+        public delegate void NonPersonDequeuedHandler(string Message);
+        public event NonPersonDequeuedHandler NonPersonDequeued;
+
+        public override object Dequeue()
+        {
+            string message = "";
+            if (this.GetType().ToString() == "Person")
+            {
+                //message = $"You successfully dequeued {this.firstName}. Their birthday is in x days!";
+                RaisePersonDequeuedEvent(message);
+            }
+            else
+            {
+                RaisePersonDequeuedEvent(message);
+            }
+            return base.Dequeue();
+        }
+
+        private void RaisePersonDequeuedEvent(string message)
+        {
+            PersonDequeued(message);
+        }
+    }
+}
